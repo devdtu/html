8cbc457356c2da805adaf2e167139a699332a3c6
root <root@ip-172-31-33-69.us-west-2.compute.internal>
2017-04-24 16:42:16 +0600

    Secon Commit

--- wp-content/themes/louis/footer.php
+++ wp-content/themes/louis/footer.php
@@ -1,45 +1,9 @@
+<html>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
-<div class="footer">
-      <div class="col-sm-1 col-sm-offset-1">
-       <h4 class="quicks" > Quick Links  </h4>
-        <p class="contact-text"><a href="http://www.myhq.in">About</a></p> 
-	<p class="contact-text"><a href="http://myhq.in/#locations">Locations</a></p> 
-	<p class="contact-text"><a href="http://myhq.in/events">Events</a></p>
-	<p class="contact-text"><a href="localhost">Blog</a></p>
-	
-    </div>
-    <div class="col-sm-2 site-map ">
-	
-	<p class="contact-text"><a href="http://myhq.in/#app-promotion">Download App</a></p>
-	<p class="contact-text"><a href="http://myhq.in/faq">FAQ</a></p>
-	<p class="contact-text"><a href="http://myhq.in/privacy">Privacy Policy</a></p>
-     
-    </div> 
-   
-    <div class="col-sm-3 col-sm-offset-1" id="contactus">
-      <h4>Contact Us</h4>
-      <i class="fa fa-2x fa-envelope"></i><span><a style="color:#fff;" class="contact-text" href="mailto:contact@myhq.in">contact@myhq.in</a><span>
-        <br>
-        <i class="fa fa-2x fa-phone"></i><span class="contact-text">+91 920 500 6361</span>
-      <br>
-      <div class="social-icons">
-        <a href="https://www.facebook.com/myhqspace" target="_blank"><i class="fa fa-2x fa-facebook-square"></i></a>
-        <a href="https://twitter.com/myhqspace" target="_blank"><i class="fa fa-2x fa-twitter-square"></i></a>
-        <a href="https://www.linkedin.com/company/myhqspace" target="_blank"><i class="fa fa-2x fa-linkedin-square"></i></a>
-        <a href="https://www.instagram.com/myhq.in" target="_blank"><i class="fa fa-2x fa-instagram"></i></a>
-      </div>
-    </div>
- 
 
 
-
-  </div>
-</section>
 <style>
-html {
-margin-top:0px !important;
-overflow-x: hidden;
-}
+
 @media(max-width:768px){
 .quicks
 {
@@ -115,13 +79,70 @@ text-align: center;
 .page-scroll.black {
   color: #222 !important;
 }
-
-.col-sm-offset-1 {
-    margin-left: 18%;
-}
-
 </style>
-             
 
-</div></div>
 
+<body>
+
+<div id="footer">
+	<div class="wrapper">
+		<div class="row">
+
+			<div class="footer">
+      <div class="col-sm-1 col-sm-offset-1">
+       <h4 class="quicks" > Quick Links  </h4>
+        <p class="contact-text"><a href="http://www.myhq.in">About</a></p> 
+	<p class="contact-text"><a href="http://myhq.in/#locations">Locations</a></p> 
+	<p class="contact-text"><a href="http://myhq.in/events">Events</a></p>
+	<p class="contact-text"><a href="localhost">Blog</a></p>
+	
+    </div>
+    <div class="col-sm-2 site-map ">
+	
+	<p class="contact-text"><a href="http://myhq.in/#app-promotion">Download App</a></p>
+	<p class="contact-text"><a href="http://myhq.in/faq">FAQ</a></p>
+	<p class="contact-text"><a href="http://myhq.in/privacy">Privacy Policy</a></p>
+     
+    </div> 
+   
+    <div class="col-sm-3 col-sm-offset-1" id="contactus">
+      <h4>Contact Us</h4>
+      <i class="fa fa-2x fa-envelope"></i><span><a style="color:#fff;" class="contact-text" href="mailto:contact@myhq.in">contact@myhq.in</a><span>
+        <br>
+        <i class="fa fa-2x fa-phone"></i><span class="contact-text">+91 920 500 6361</span>
+      <br>
+      <div class="social-icons">
+        <a href="https://www.facebook.com/myhqspace" target="_blank"><i class="fa fa-2x fa-facebook-square"></i></a>
+        <a href="https://twitter.com/myhqspace" target="_blank"><i class="fa fa-2x fa-twitter-square"></i></a>
+        <a href="https://www.linkedin.com/company/myhqspace" target="_blank"><i class="fa fa-2x fa-linkedin-square"></i></a>
+        <a href="https://www.instagram.com/myhq.in" target="_blank"><i class="fa fa-2x fa-instagram"></i></a>
+      </div>
+    </div>
+   
+
+
+  </div>
+  <div class="footer-base">
+    <div class="col-sm-5 col-sm-offset-1">
+      <span class="footer-text">  &copy; Copyright 2016 Justwork Technologies Pvt. Ltd. - All Rights Reserved</span>
+    </div>
+    <div class="col-sm-3 col-sm-offset-2">
+      <span class="footer-text"> <a ui-sref="privacypolicy" style="color:#ffffff">Privacy Policy</a> | <a ui-sref="refundpolicy" style="color:#ffffff">Refund Policy</a> | <a ui-sref="termsncondition" style="color:#ffffff">Terms and Condition</a>
+        </span>
+    </div>
+  </div>
+		</div>
+	</div>
+
+
+
+	<!-- End wrapper -->
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+
+
+
+</html>
